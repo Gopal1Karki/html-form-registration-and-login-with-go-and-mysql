d8a722f1431d1c117f59ed42ef8d9b936171b211
Gopal1Karki <karkipawan305@gmail.com>
2023-02-27 20:09:54 +0545

    initialcommit

--- back.go
+++ back.go
@@ -30,30 +30,57 @@ func init() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	tpl = template.Must(template.ParseFiles("signup.html"))
 
 }
 
 func main() {
+	http.HandleFunc("/", home)
 	http.HandleFunc("/signup", signup)
+	http.HandleFunc("/login", login)
 	http.ListenAndServe(":8080", nil)
 }
 
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the homepage!")
+}
+
 func signup(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		tpl.Execute(w, nil)
+	tpl = template.Must(template.ParseFiles("signup.html"))
+	if r.Method == "POST" {
+		user := User{
+			Username: r.FormValue("username"),
+			Password: r.FormValue("password"),
+			Email:    r.FormValue("email"),
+		}
+		_, err := db.Exec(`insert into info (username,email,password) value (?,?,?)`, user.Username, user.Email, user.Password)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "User created successfully!")
 		return
 	}
 
-	user := User{
-		Username: r.FormValue("username"),
-		Password: r.FormValue("password"),
-		Email:    r.FormValue("email"),
-	}
-	_, err := db.Exec(`insert into info (username,email,password) value (?,?,?)`, user.Username, user.Email, user.Password)
-	if err != nil {
-		log.Fatal(err)
+	tpl.Execute(w, nil)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	tpl = template.Must(template.ParseFiles("login.html"))
+	if r.Method == "POST" {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		var storedPassword string
+		err := db.QueryRow(`SELECT password FROM info WHERE username = ?`, username).Scan(&storedPassword)
+		if err != nil {
+			fmt.Fprintf(w, "Login failed!")
+			return
+		}
+		if password == storedPassword {
+			fmt.Fprintf(w, "Login successful!")
+			return
+		}
+		fmt.Fprintf(w, "Login failed!")
+		return
 	}
 
-	fmt.Fprintf(w, "User created successfully!")
+	tpl.Execute(w, nil)
 }
